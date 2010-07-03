9df78608db475cfab7b49b45f14a96255deeb045
Chris Ortman <chrisortman@gmail.com>
2010-07-03 16:06:32 -0500

    First commit.
    Notice how it doesn't compile because of name.

---
+++ YourLastCustomCollection/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YourLastCustomCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList customers = new ArrayList();
+            customers.Add(new Customer("Chris"));
+            customers.Add(new Customer("Missy"));
+            customers.Add(new Customer("Lincoln"));
+
+            
+            for(int i=0; i<customers.Count; i++)
+            {
+                Console.WriteLine("Customer {0}'s name is {1}",i,customers[i].Name);
+            }
+        }
+    }
+
+    class Customer
+    {
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
